49ec9943202559ab009f8ab5f5d7e123126a0659
Robert Tilton <robtilton2086@gmail.com>
2021-02-21 12:40:52 -0500

    built out most of the core functionality of the site, next step is to start propagating based off the search results

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,23 @@
 var searchButton = document.getElementById("city-search");
+var searchContent = document.getElementById("search-history");
+var dateName = document.getElementById("date-name");
+var Temperature = document.getElementById("temper");
+var Humidity = document.getElementById("humidity");
+var windSpeed = document.getElementById("wind-speed");
+var uvIndex = document.getElementById("uv-index");
+var searchResults= [];
 
 searchButton.addEventListener("click", function () {
     var cityInput = document.getElementById("city-input").value;
     var latitude;
     var longitude;
+
+    searchResults.push(cityInput);
+    console.log(searchResults);
+    var searchPropagate = document.createElement('p');
+    var parsedResults = searchResults[searchResults.length-1]
+    searchPropagate.textContent = parsedResults;
+    searchContent.appendChild(searchPropagate);
 
     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityInput}&appid=96b0eafea01d741545d3918386fdb490`, {
     })
@@ -15,5 +29,6 @@ searchButton.addEventListener("click", function () {
             fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=96b0eafea01d741545d3918386fdb490`, {
             }).then(response => response.json())
                 .then(data => console.log(data));
+
         })
-})
+    })
